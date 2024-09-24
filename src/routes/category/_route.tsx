9d6e6bec57e48175ea0516c960cd0cae77892c13
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-09-25 01:44:19 +0300

    Category Update

--- src/routes/category/_route.tsx
+++ src/routes/category/_route.tsx
@@ -1,18 +1,17 @@
 import "react-image-crop/dist/ReactCrop.css";
 
-import { Button, Snackbar, Typography } from "@mui/material";
+import { Button, CircularProgress, Modal, Snackbar, Typography } from "@mui/material";
 import { GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
-import { useCreateCategoryMutation, useGetCategoryListQuery } from "@api/admin/category";
+import { useCreateCategoryMutation, useGetCategoryListQuery, useUpdateCategoryMutation } from "@api/admin/category";
+import { useEffect, useMemo, useState } from "react";
 
 import ActionDialog from "@components/ActionDialog";
 import { Add } from "@mui/icons-material";
 import AdminTable from "../table";
 import { CategoryCreateForm } from "./CreateForm";
-import { CategoryCreateSchema } from "@schemas/Category";
+import { CategoryUpdateForm } from "./UpdateForm";
 import { LoadingSpinner } from "@components/LoadingSpinner";
 import ManagementModal from "../managementModal";
-import { useState } from "react";
-import { z } from "zod";
 
 const columns: GridColDef[] = [
 	{ field: "createdAt", headerName: "Создан", type: "dateTime" },
@@ -22,62 +21,113 @@ const columns: GridColDef[] = [
 ];
 
 export default function Category() {
-	const { data, isLoading } = useGetCategoryListQuery();
-	const [createCategory] = useCreateCategoryMutation();
+	const { data: categoryList, isLoading: categoryListIsLoading } = useGetCategoryListQuery();
+	const [createCategory, { isLoading: createIsLoading, isSuccess: createSuccess, error: createError }] =
+		useCreateCategoryMutation();
+	const [updateCategory, { isLoading: updateIsLoading, isSuccess: updateSuccess, error: updateError }] =
+		useUpdateCategoryMutation();
 
 	const [deletionDialogOpened, setDeletionDialogOpened] = useState<boolean>(false);
 	const [createModalOpened, setCreateModalOpened] = useState<boolean>(false);
 	const [updateModalOpened, setUpdateModalOpened] = useState<boolean>(false);
-	const [successSnackBarOpened, setSuccessSnackBarOpened] = useState<boolean>(false);
+
+	const [createSuccessSnackBarOpened, setCreateSuccessSnackBarOpened] = useState<boolean>(false);
+	const [createErrorSnackBarOpened, setCreateErrorSnackBarOpened] = useState<boolean>(false);
+
+	const [updateSuccessSnackBarOpened, setUpdateSuccessSnackBarOpened] = useState<boolean>(false);
+	const [updateErrorSnackBarOpened, setUpdateErrorSnackBarOpened] = useState<boolean>(false);
 
 	const [selectedItemIds, setSelectedItemIds] = useState<GridRowSelectionModel>([]);
 
-	const handleCategoryCreate = async (data: z.infer<typeof CategoryCreateSchema>) => {
-		const response = await createCategory(data);
-		console.debug("CategoryCreate response:", response);
-		setSuccessSnackBarOpened(true);
-		setCreateModalOpened(false);
-	};
+	const selectedCategory = useMemo(() => {
+		if (!categoryList) return null;
+		return categoryList.items.find((category) => category.id === selectedItemIds[0]);
+	}, [categoryList, selectedItemIds]);
 
-	const handleCategoryUpdate = () => {
-		throw new Error("Not implemented");
-		setSuccessSnackBarOpened(true);
-		setUpdateModalOpened(false);
-	};
+	const showLoadingOverlay = useMemo(() => {
+		return createIsLoading || updateIsLoading;
+	}, [createIsLoading, updateIsLoading]);
 
-	// const handleDeleteFaqItems = async () => {
-	//     console.log("Submitting FaqItemsDelete, ids:", selectedItemIds);
-	//     const response = await adminApiClient.deleteFAQItems(selectedItemIds.map(String));
-	//     console.log("FaqItemsDelete response:", response);
-	//     revalidator.revalidate();
-	//     setSuccessSnackBarOpened(true);
-	//     setDeletionDialogOpened(false);
-	// };
+	useEffect(() => {
+		if (createSuccess) {
+			setCreateSuccessSnackBarOpened(true);
+			setCreateModalOpened(false);
+		}
+	}, [createSuccess]);
+
+	useEffect(() => {
+		if (createError) {
+			setCreateErrorSnackBarOpened(true);
+			setCreateModalOpened(false);
+		}
+	}, [createError]);
+
+	useEffect(() => {
+		if (updateSuccess) {
+			setUpdateSuccessSnackBarOpened(true);
+			setUpdateModalOpened(false);
+		}
+	}, [updateSuccess]);
+
+	useEffect(() => {
+		if (updateError) {
+			setUpdateErrorSnackBarOpened(true);
+			setUpdateModalOpened(false);
+		}
+	}, [updateError]);
 
 	return (
 		<div className="h-100v d-f fd-c px-3 pt-1 pb-4">
+			<Modal open={showLoadingOverlay}>
+				<div className="w-100v h-100v d-f ai-c jc-c" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}>
+					<CircularProgress />
+				</div>
+			</Modal>
 			<Snackbar
-				open={successSnackBarOpened}
-				autoHideDuration={4000}
-				onClose={() => setSuccessSnackBarOpened(false)}
-				message="Изменения сохранены"
+				open={createSuccessSnackBarOpened}
+				autoHideDuration={2000}
+				onClose={() => setCreateSuccessSnackBarOpened(false)}
+				message="Категория успешно создана"
 			/>
+			<Snackbar
+				open={createErrorSnackBarOpened}
+				autoHideDuration={2000}
+				onClose={() => setCreateErrorSnackBarOpened(false)}
+				message="Произошла ошибка при создании категории"
+			/>
+
+			<Snackbar
+				open={updateSuccessSnackBarOpened}
+				autoHideDuration={2000}
+				onClose={() => setUpdateSuccessSnackBarOpened(false)}
+				message="Категория успешно обновлена"
+			/>
+			<Snackbar
+				open={updateErrorSnackBarOpened}
+				autoHideDuration={2000}
+				onClose={() => setUpdateErrorSnackBarOpened(false)}
+				message="Произошла ошибка при обновлении категории"
+			/>
+
 			<ManagementModal
 				title="Создать категорию"
 				opened={createModalOpened}
 				onClose={() => setCreateModalOpened(false)}
 			>
-				<CategoryCreateForm onSubmit={handleCategoryCreate} />
+				<CategoryCreateForm onSubmit={createCategory} />
 			</ManagementModal>
 			<ManagementModal
 				title="Изменить категорию"
 				opened={updateModalOpened}
 				onClose={() => setUpdateModalOpened(false)}
 			>
-				<div>
-					Not implemented
-					<button onClick={handleCategoryUpdate}>Submit</button>
-				</div>
+				<LoadingSpinner isLoading={categoryListIsLoading}>
+					{!selectedCategory ? (
+						<Typography variant="h6">Что-то пошло не так</Typography>
+					) : (
+						<CategoryUpdateForm category={selectedCategory} onSubmit={updateCategory} />
+					)}
+				</LoadingSpinner>
 			</ManagementModal>
 			<ActionDialog
 				title="Удалить выбранные категории?"
@@ -96,7 +146,7 @@ export default function Category() {
 				<div>
 					<Typography variant="h5">Категории</Typography>
 					<Typography variant="body2" color="typography.secondary">
-						Количество: {data?.items.length || 0}
+						Количество: {categoryList?.items.length || 0}
 					</Typography>
 				</div>
 				<Button variant="contained" onClick={() => setCreateModalOpened(true)}>
@@ -104,15 +154,15 @@ export default function Category() {
 					Добавить категорию
 				</Button>
 			</div>
-			<LoadingSpinner isLoading={isLoading}>
-				{!data ? (
+			<LoadingSpinner isLoading={categoryListIsLoading}>
+				{!categoryList ? (
 					<div className="w-100 h-100v d-f ai-c jc-c">
 						<Typography variant="h5">Что-то пошло не так</Typography>
 					</div>
 				) : (
 					<AdminTable
 						columns={columns}
-						data={data.items}
+						data={categoryList.items}
 						onRowSelect={setSelectedItemIds}
 						selectedRows={selectedItemIds}
 						headerButtons={
