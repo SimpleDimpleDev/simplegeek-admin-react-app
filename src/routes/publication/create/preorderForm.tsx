4e1979a9933e5e43d8bbd548a5b7e6e3916b20fe
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-17 02:21:11 +0300

    Debug

--- src/routes/publication/create/preorderForm.tsx
+++ src/routes/publication/create/preorderForm.tsx
@@ -164,18 +164,20 @@ const ItemForm: React.FC<ItemFormProps> = ({
 
 	const isCredit = watch(`items.${index}.isCredit`);
 	const creditDeposit = watch(`items.${index}.creditDeposit`);
+	const creditPayments = watch(`items.${index}.creditPayments`);
 
 	const quantityIsUnlimited = watch(`items.${index}.unlimitedQuantity`);
 
 	useEffect(() => {
 		if (isCredit) {
-			const creditTotal =
-				(creditDeposit ? Number(creditDeposit) : 0) +
-				creditPaymentsFields.map((payment) => Number(payment.sum)).reduce((sum, current) => sum + current, 0);
-
+			const creditDepositValue = creditDeposit ? Number(creditDeposit) : 0;
+			const creditPaymentsValue = creditPayments.map((payment) => Number(payment.sum)).reduce((sum, current) => sum + current, 0);
+			console.log({ creditDepositValue, creditPaymentsValue });
+			const creditTotal = creditDepositValue + creditPaymentsValue;
+			console.log({ creditTotal });
 			setValue(`items.${index}.price`, creditTotal.toString());
 		}
-	}, [index, setValue, isCredit, creditDeposit, creditPaymentsFields]);
+	}, [index, setValue, isCredit, creditDeposit, creditPayments]);
 
 	return (
 		<div key={index} className="gap-2 py-2 w-100 d-f fd-r">
