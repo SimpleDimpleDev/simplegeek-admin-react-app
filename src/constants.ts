d73b5c8c737ef73eab5f308516e77d8a5bbfd657
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-20 17:54:59 +0300

    Datagrid select

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,35 @@
+import { DeliveryService } from "@appTypes/Delivery";
+import { OrderStatus } from "@appTypes/Order";
+import { PreorderStatus } from "@appTypes/Preorder";
 import { UserRoleSchema } from "@schemas/User";
 import { z } from "zod";
 
 export const userRoleTitles: Map<z.infer<typeof UserRoleSchema>, string> = new Map([
 	["Admin", "Администратор"],
 	["Customer", "Покупатель"],
+]);
+
+export const orderStatusTitles: Map<OrderStatus, string> = new Map([
+	["CANCELLED", "Отменен"],
+	["UNPAID", "Не оплачен"],
+	["ACCEPTED", "Оформлен"],
+	["DELIVERY", "Передан в доставку"],
+	["READY_FOR_PICKUP", "Готов к выдаче"],
+	["FINISHED", "Доставлен"],
+]);
+
+export const preorderStatusTitles: Map<PreorderStatus, string> = new Map([
+	["NEW", "Новый"],
+	["FUNDING", "Сбор"],
+	["WAITING_FOR_RELEASE", "Ожидание релиза"],
+	["FOREIGN_SHIPPING", "Доставка на зарубежный склад"],
+	["LOCAL_SHIPPING", "Доставка на склад РФ"],
+	["DISPATCH", "На складе"],
+	["FINISHED", "Завершен"],
+]);
+
+export const deliveryServiceTitles: Map<DeliveryService | "UNASSIGNED", string> = new Map([
+	["UNASSIGNED", "Не оформлена"],
+	["SELF_PICKUP", "Самовывоз"],
+	["CDEK", "СДЕК"],
 ]);
--- src/routes/order/table/_route.tsx
+++ src/routes/order/table/_route.tsx
@@ -1,20 +1,14 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import { GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
+import { deliveryServiceTitles, orderStatusTitles } from "src/constants";
 import { useMemo, useState } from "react";
 
 import AdminTable from "@components/ManagementTable";
-import { DeliveryService } from "@appTypes/Delivery";
 import { LoadingSpinner } from "@components/LoadingSpinner";
 import { OrderGet } from "@appTypes/Order";
 import { orderStatusBadges } from "@components/Badges";
 import { useGetOrderListQuery } from "@api/admin/order";
 import { useNavigate } from "react-router-dom";
-
-const deliveryServiceMapping: Record<DeliveryService | "UNASSIGNED", string> = {
-	UNASSIGNED: "Не оформлена",
-	SELF_PICKUP: "Самовывоз",
-	CDEK: "СДЕК",
-};
 
 const selfPickupColumns: GridColDef<OrderGet>[] = [
 	{
@@ -30,14 +24,25 @@ const selfPickupColumns: GridColDef<OrderGet>[] = [
 		renderCell: ({ row: { status } }) => {
 			return <div className="w-100 h-100 ai-c d-f jc-c">{orderStatusBadges[status]}</div>;
 		},
+		type: "singleSelect",
+		valueOptions: Array.from(orderStatusTitles.entries()).map(([status, title]) => ({
+			value: status,
+			label: title,
+		})),
 	},
 	{
 		field: "deliveryService",
 		headerName: "Сервис доставки",
+		valueGetter: (_, row) => (row.delivery ? row.delivery.service : "UNASSIGNED"),
 		renderCell: ({ row: { delivery } }) => {
-			if (!delivery) return deliveryServiceMapping["UNASSIGNED"];
-			return deliveryServiceMapping[delivery.service as DeliveryService];
+			if (!delivery) return deliveryServiceTitles.get("UNASSIGNED");
+			return deliveryServiceTitles.get(delivery.service);
 		},
+		type: "singleSelect",
+		valueOptions: Array.from(deliveryServiceTitles.entries()).map(([service, title]) => ({
+			value: service,
+			label: title,
+		})),
 	},
 	{
 		field: "preorder",
@@ -56,16 +61,6 @@ export default function OrderTableRoute() {
 
 	const { data: orderList, isLoading: orderListIsLoading } = useGetOrderListQuery();
 
-	const [deliveryService, setDeliveryService] = useState<DeliveryService | "UNASSIGNED">("SELF_PICKUP");
-
-	const deliveryServiceOrders = useMemo(
-		() =>
-			orderList?.items.filter((item) =>
-				item.delivery ? item.delivery.service === deliveryService : deliveryService === "UNASSIGNED"
-			),
-		[orderList, deliveryService]
-	);
-
 	const [selectedItemIds, setSelectedItemIds] = useState<GridRowSelectionModel>([]);
 
 	const selectedOrder = useMemo(() => {
@@ -73,11 +68,6 @@ export default function OrderTableRoute() {
 		const selectedItemId = selectedItemIds[0];
 		return orderList?.items.find((order) => order.id === selectedItemId) || null;
 	}, [selectedItemIds, orderList]);
-
-	const handleChangeDeliveryService = (service: DeliveryService | "UNASSIGNED") => {
-		setSelectedItemIds([]);
-		setDeliveryService(service);
-	};
 
 	return (
 		<div className="px-3 pt-1 pb-4 h-100v d-f fd-c">
@@ -109,39 +99,22 @@ export default function OrderTableRoute() {
 					</Typography>
 				</div>
 			</div>
-			<FormControl fullWidth>
-				<InputLabel id="delivery-service-label">Сервис доставки</InputLabel>
-				<Select
-					value={deliveryService}
-					onChange={(e) => handleChangeDeliveryService(e.target.value as DeliveryService | "UNASSIGNED")}
-					labelId="delivery-service-label"
-					label="Сервис доставки"
-					fullWidth
-					variant="outlined"
-				>
-					{Object.entries(deliveryServiceMapping).map(([key, value]) => (
-						<MenuItem key={key} value={key}>
-							{value}
-						</MenuItem>
-					))}
-				</Select>
-			</FormControl>
 			<LoadingSpinner isLoading={orderListIsLoading}>
-				{!deliveryServiceOrders ? (
+				{!orderList ? (
 					<div className="w-100 h-100v ai-c d-f jc-c">
 						<Typography variant="h5">Что-то пошло не так</Typography>
 					</div>
 				) : (
 					<AdminTable
 						columns={selfPickupColumns}
-						data={deliveryServiceOrders}
+						data={orderList.items}
 						onRowSelect={setSelectedItemIds}
 						selectedRows={selectedItemIds}
 						headerButtons={
 							<>
 								<Button
 									variant="contained"
-									disabled={!selectedItemIds.length || deliveryService !== "CDEK"}
+									disabled={!selectedItemIds.length}
 									onClick={() => {
 										alert("Not implemented yet");
 									}}
