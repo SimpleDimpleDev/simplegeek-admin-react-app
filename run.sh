9529f78595469eaad517389b521e99315f2b0050
SilentCodeSamurai <silentcodesamurai@gmail.com>
2024-10-23 07:35:43 +0300

    Useless rm

--- run.sh
+++
@@ -1,5 +0,0 @@
-docker run -d \
-    --name simplegeek-admin-react-app-server \
-    --network intranet \
-    --network-alias simplegeek-admin-react-app-server \
-    -p 3040:3040 simplegeek-admin-react-app-server
--- stop.sh
+++
@@ -1,2 +0,0 @@
-docker stop simplegeek-admin-react-app-server
-docker rm simplegeek-admin-react-app-server
