63c36a580fa7b906202631f06b193ef85846b174
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-23 22:24:20 +0300

    fix: product template forms

--- src/routes/product/template/forms/CreateForm.tsx
+++ src/routes/product/template/forms/CreateForm.tsx
@@ -243,101 +243,103 @@ export const ProductTemplateCreateForm: React.FC<ProductTemplateCreateFormProps>
 				<div className="gap-1 d-f fd-c">
 					<Typography variant="subtitle0">Физические свойства</Typography>
 					<div className="gap-2 d-f fd-c">
-						// TODO: Optional physical properties feature
+						{/* TODO: Optional physical properties feature */}
 						{selectedPhysicalProperties === null ? (
 							<Typography variant="body1" sx={{ color: "typography.secondary" }}>
 								Товар невозможно опубликовать в розницу без указания физических свойств
 							</Typography>
 						) : (
-							<div className="gap-2 d-f fd-r">
-								<Controller
-									name="data.physicalProperties.length"
-									control={control}
-									render={({ field: { onChange, value }, fieldState: { error } }) => (
-										<TextField
-											type="string"
-											label="Длина, сантиметров"
-											variant="outlined"
-											fullWidth
-											value={value}
-											onChange={handleIntChange(onChange)}
-											error={!!error}
-											helperText={error?.message}
-											slotProps={{
-												input: {
-													endAdornment: <Typography variant="body1">см</Typography>,
-												},
-											}}
-										/>
-									)}
-								/>
+							<>
+								<div className="gap-2 d-f fd-r">
+									<Controller
+										name="data.physicalProperties.length"
+										control={control}
+										render={({ field: { onChange, value }, fieldState: { error } }) => (
+											<TextField
+												type="string"
+												label="Длина, сантиметров"
+												variant="outlined"
+												fullWidth
+												value={value}
+												onChange={handleIntChange(onChange)}
+												error={!!error}
+												helperText={error?.message}
+												slotProps={{
+													input: {
+														endAdornment: <Typography variant="body1">см</Typography>,
+													},
+												}}
+											/>
+										)}
+									/>
+									<Controller
+										name="data.physicalProperties.width"
+										control={control}
+										render={({ field: { onChange, value }, fieldState: { error } }) => (
+											<TextField
+												type="string"
+												label="Ширина, сантиметров"
+												variant="outlined"
+												fullWidth
+												value={value}
+												onChange={handleIntChange(onChange)}
+												error={!!error}
+												helperText={error?.message}
+												slotProps={{
+													input: {
+														endAdornment: <Typography variant="body1">см</Typography>,
+													},
+												}}
+											/>
+										)}
+									/>
+								</div>
+								<div className="gap-2 d-f fd-r">
+									<Controller
+										name="data.physicalProperties.height"
+										control={control}
+										render={({ field: { onChange, value }, fieldState: { error } }) => (
+											<TextField
+												type="string"
+												label="Высота, сантиметров"
+												variant="outlined"
+												fullWidth
+												value={value}
+												onChange={handleIntChange(onChange)}
+												error={!!error}
+												helperText={error?.message}
+												slotProps={{
+													input: {
+														endAdornment: <Typography variant="body1">см</Typography>,
+													},
+												}}
+											/>
+										)}
+									/>
 
-								<Controller
-									name="data.physicalProperties.width"
-									control={control}
-									render={({ field: { onChange, value }, fieldState: { error } }) => (
-										<TextField
-											type="string"
-											label="Ширина, сантиметров"
-											variant="outlined"
-											fullWidth
-											value={value}
-											onChange={handleIntChange(onChange)}
-											error={!!error}
-											helperText={error?.message}
-											slotProps={{
-												input: {
-													endAdornment: <Typography variant="body1">см</Typography>,
-												},
-											}}
-										/>
-									)}
-								/>
-
-								<Controller
-									name="data.physicalProperties.height"
-									control={control}
-									render={({ field: { onChange, value }, fieldState: { error } }) => (
-										<TextField
-											type="string"
-											label="Высота, сантиметров"
-											variant="outlined"
-											fullWidth
-											value={value}
-											onChange={handleIntChange(onChange)}
-											error={!!error}
-											helperText={error?.message}
-											slotProps={{
-												input: {
-													endAdornment: <Typography variant="body1">см</Typography>,
-												},
-											}}
-										/>
-									)}
-								/>
-
-								<Controller
-									name="data.physicalProperties.weight"
-									control={control}
-									render={({ field: { onChange, value }, fieldState: { error } }) => (
-										<TextField
-											type="string"
-											label="Масса, грамм"
-											variant="outlined"
-											fullWidth
-											value={value}
-											onChange={handleIntChange(onChange)}
-											error={!!error}
-											helperText={error?.message}
-											slotProps={{
-												input: {
-													endAdornment: <Typography variant="body1">г</Typography>,
-												},
-											}}
-										/>
-									)}
-								/>
-							</div>
+									<Controller
+										name="data.physicalProperties.weight"
+										control={control}
+										render={({ field: { onChange, value }, fieldState: { error } }) => (
+											<TextField
+												type="string"
+												label="Масса, грамм"
+												variant="outlined"
+												fullWidth
+												value={value}
+												onChange={handleIntChange(onChange)}
+												error={!!error}
+												helperText={error?.message}
+												slotProps={{
+													input: {
+														endAdornment: <Typography variant="body1">г</Typography>,
+													},
+												}}
+											/>
+										)}
+									/>
+								</div>
+							</>
 						)}
 					</div>
 				</div>
