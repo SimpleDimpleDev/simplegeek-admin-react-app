ced775ada8a30de174cb4b0f22b1a20cd6abcbe0
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-26 02:15:23 +0300

    feat: homepage metrics

--- src/schemas/Metrics.ts
+++ src/schemas/Metrics.ts
@@ -1,5 +1,23 @@
+import { ISOToDateSchema } from "./Primitives";
 import { z } from "zod";
 
-export const MetricsSchema = z.object({
-    
+export const DataPointSchema = z.object({
+	date: ISOToDateSchema,
+	value: z.number(),
+});
+
+export const MetricsFilterSchema = z.object({
+	year: z.number(),
+	month: z.number(),
+});
+
+export const MetricsChartsGetSchema = z.object({
+	orderQuantityDataSet: DataPointSchema.array(),
+	incomeDataSet: DataPointSchema.array(),
+});
+
+export const MetricsSummaryGetSchema = z.object({
+	ordersToProcess: z.number(),
+    totalOrders: z.number(),
+	totalIncome: z.number(),
 });
