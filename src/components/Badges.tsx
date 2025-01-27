d9c15ab271818b9e34140b58b01d379932300be6
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-27 05:32:24 +0300

    upd: badges

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,29 +1,29 @@
+import { Chip } from "@mui/material";
 import { InvoiceStatus } from "@appTypes/Payment";
 import { OrderStatus } from "@appTypes/Order";
 import { PreorderStatus } from "@appTypes/Preorder";
-import { StatusBadge } from "./StatusBadge";
 
 export const orderStatusBadges: Record<OrderStatus, JSX.Element> = {
-	CANCELLED: <StatusBadge color="icon.secondary">Отменен</StatusBadge>,
-	UNPAID: <StatusBadge color="icon.attention">Не оплачен</StatusBadge>,
-	ACCEPTED: <StatusBadge color="warning.main">Оформлен</StatusBadge>,
-	DELIVERY: <StatusBadge color="rgb(0, 175, 134)">Передан в доставку</StatusBadge>,
-	READY_FOR_PICKUP: <StatusBadge color="icon.success">Готов к выдаче</StatusBadge>,
-	FINISHED: <StatusBadge color="icon.success">Завершен</StatusBadge>,
+	CANCELLED: <Chip color="secondary" label="Отменен" />,
+	UNPAID: <Chip color="error" label="Не оплачен" />,
+	ACCEPTED: <Chip color="warning" label="Оформлен" />,
+	DELIVERY: <Chip color="info" label="Передан в доставку" />,
+	READY_FOR_PICKUP: <Chip color="success" label="Готов к выдаче" />,
+	FINISHED: <Chip color="success" label="Выдан" />,
 };
 
 export const preorderStatusBadges: Record<PreorderStatus, JSX.Element> = {
-	NEW: <StatusBadge color="rgb(0, 175, 134)">Новый</StatusBadge>,
-	WAITING_FOR_RELEASE: <StatusBadge color="icon.brandSecondary">Ожидание релиза</StatusBadge>,
-	FOREIGN_SHIPPING: <StatusBadge color="icon.brandSecondary">Доставка на зарубежный склад</StatusBadge>,
-	LOCAL_SHIPPING: <StatusBadge color="icon.brandSecondary">Доставка на склад РФ</StatusBadge>,
-	DISPATCH: <StatusBadge color="icon.success">На складе</StatusBadge>,
-	FINISHED: <StatusBadge color="icon.secondary">Завершен</StatusBadge>,
+	NEW: <Chip color="primary" label="Новый" />,
+	WAITING_FOR_RELEASE: <Chip color="warning" label="Ожидание релиза" />,
+	FOREIGN_SHIPPING: <Chip color="info" label="Доставка на зарубежный склад" />,
+	LOCAL_SHIPPING: <Chip color="info" label="Доставка на склад РФ" />,
+	DISPATCH: <Chip color="success" label="На складе" />,
+	FINISHED: <Chip color="success" label="Завершен" />,
 };
 
 export const InvoiceStatusBadges: Record<InvoiceStatus, JSX.Element> = {
-	PAID: <StatusBadge color="icon.success">Оплачен</StatusBadge>,
-	UNPAID: <StatusBadge color="icon.attention">Не оплачен</StatusBadge>,
-	REFUNDED: <StatusBadge color="icon.secondary">Произведён возврат</StatusBadge>,
-	WAITING: <StatusBadge color="icon.brandSecondary">Ожидание банка</StatusBadge>,
+	PAID: <Chip color="success" label="Оплачен" />,
+	UNPAID: <Chip color="error" label="Не оплачен" />,
+	REFUNDED: <Chip color="secondary" label="Произведён возврат" />,
+	WAITING: <Chip color="warning" label="Ожидание ответа банка" />,
 };
