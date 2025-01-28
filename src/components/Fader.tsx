a36b2a0daa4bebbf9f4629c4bdd69a64467872c9
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-28 22:22:29 +0300

    upd: Fader

--- src/components/Fader.tsx
+++ src/components/Fader.tsx
@@ -1,38 +1,52 @@
 import React, { useEffect, useState } from "react";
 
 interface FaderProps {
-	deps: React.DependencyList; // Optional array of dependencies
-	timeout?: number; // Optional timeout in milliseconds before fading out
-	children: React.ReactNode; // Children to render after fade out
+	deps?: React.DependencyList;
+	timeout?: number;
+	children: React.ReactNode;
 }
 
 const Fader: React.FC<FaderProps> = ({ deps = [], timeout = 500, children }) => {
-	const [isFading, setIsFading] = useState(false);
+	const [renderChildren, setRenderChildren] = useState(false);
+	const [isFading, setIsFading] = useState(true);
 
 	useEffect(() => {
-		// Start fading in when the component mounts or deps change
+		setRenderChildren(false);
 		setIsFading(true);
-		// Set a timeout to fade out after the specified duration
+
+		const renderChildrenTimeout = setTimeout(() => {
+			setRenderChildren(true);
+		}, 100);
+
 		const fadeOutTimeout = setTimeout(() => {
 			setIsFading(false);
 		}, timeout);
-		// Cleanup function to clear the timeout
+
 		return () => {
+			clearTimeout(renderChildrenTimeout);
 			clearTimeout(fadeOutTimeout);
 		};
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [...deps, timeout]); // Re-run effect when deps change
+	}, [...deps, timeout]);
 
-	return isFading ? (
-		<div
-			className="bg-secondary w-100 h-100"
-			style={{
-				transition: `opacity ${timeout}ms ease-in-out`,
-				opacity: isFading ? 1 : 0,
-			}}
-		/>
-	) : (
-		children
+	return (
+		<div style={{ width: "100%", height: "100%", position: "relative" }}>
+			{isFading && (
+				<div
+					className="bg-secondary"
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						bottom: 0,
+						right: 0,
+						transition: `opacity ${timeout}ms ease-in-out`,
+						opacity: isFading ? 1 : 0,
+					}}
+				/>
+			)}
+			{renderChildren && children}
+		</div>
 	);
 };
 
